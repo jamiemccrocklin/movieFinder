fae69a1b83371e1b47b8ea4604ee1270eebff7d4
Jamie McCrocklin <jamiemccrocklin@gmail.com>
2019-06-28 13:28:22 -0400

    Add datepicker functionality to landing page.
    May require some debugging later. Had to add jQuery links to form tag in HTML

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// const amcApiKey = 'FCBFBDED-2C13-4204-A0DD-73DDF3610237'; 
+// const amcBaseURL = 'https://developer.nps.gov/api/v1/parks';
+// const amcTheatersbyZip = '/v2/location-suggestions/?query=66062';
+// const amcShowtimes = '/v2/theatres/{theatreNumber}/showtimes/{date}';
+// const amcMovies = '/v2/movies/{id}';
+
+// const for selected theater number
+// const for selected data
+// const for input zip
+// const for movie ID
+ 
+// on click of calendar input, datepicker displays
+$(function() {
+    $('.calendar').datepicker();
+    console.log('datepicker running');
+  });
+
+// have a spot when shows are showing to modify date/theater? 
+
+// function watchForm() {
+//     $('form').submit(event => {
+//       event.preventDefault();
+//     });
+//   }
+  
+//   $(watchForm);
