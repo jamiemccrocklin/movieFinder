ad6b3635034c135ffa4ac23639f4df9ef58a7267
Jamie McCrocklin <jamiemccrocklin@gmail.com>
2019-06-30 10:50:37 -0400

    Add working console.log for amcTheaterSuggestions. Debug error 405

--- index.js
+++ index.js
@@ -1,35 +1,96 @@
 'use strict';
 
-// const amcApiKey = 'FCBFBDED-2C13-4204-A0DD-73DDF3610237'; 
-// const amcBaseURL = 'https://developer.nps.gov/api/v1/parks';
-// const amcTheatersbyZip = '/v2/location-suggestions/?query=66062';
+const amcApiKey = '451EB6B4-E2FD-412E-AF07-CA640853CDC3'; 
+const amcBaseURL = 'https://api.amctheatres.com';
+// const amcTheatersSuggestions = '/v2/location-suggestions/?query=40206';
+// ex https://api.amctheatres.com/v2/location-suggestions/?query=40206
+// _embedded.suggestions.https://api.amctheatres.com/rels/v2/locations.href = link to query (with lat/long) which pulls up theaters
 // const amcShowtimes = '/v2/theatres/{theatreNumber}/showtimes/{date}';
+// https://api.amctheatres.com/v2/theatres/4266/showtimes/6-30-2019
 // const amcMovies = '/v2/movies/{id}';
+const options = {
+    headers: new Headers({
+    //   "Access-Control-Allow-Origin": 'https://api.amctheatres.com',
+    //   "Vary": 'Origin',
+      "X-AMC-Vendor-Key": amcApiKey})
+  };
 
-// const for selected theater number
+
+// const for selected theater id
 // const for selected data
-// const for input zip
 // const for movie ID
- 
-// on click of calendar input, datepicker displays
-$(function() {
-    $('.calendar').datepicker();
-    console.log('datepicker running');
-    //disable historical dates
-  });
-
-$('.go-button').on('click', function() {
-    $('.landingPg').remove();
-})
 
 
-// function watchForm() {
-//     $('form').submit(event => {
-//       event.preventDefault();
-//     });
-//   }
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
+
+//JS for grabbing location suggestions
+
+function getSuggestions() {
+    const inputZip = $('.js-zip').val();
+    const params = {
+    query: inputZip
+  };
+  const queryString = formatQueryParams(params);
+  const amcTheatersSuggestions = amcBaseURL + '/v2/location-suggestions/?' + queryString;
+
+  console.log(amcTheatersSuggestions);
+
+  fetch(amcTheatersSuggestions, options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+    // console.log(responseJson._embedded.suggestions.https://api.amctheatres.com/rels/v2/locations.href)
+}
+
+//add this template to the above for an error message when ready
+//   fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error(response.statusText);
+//     })
+//     // .then(responseJson => console.log(responseJson));
+//     .then(responseJson => displayResults(responseJson));
+//     // .catch(err => {
+//     //   $('#js-error-message').text(`Something went wrong: ${err.message}`);
+//     // });
+// }
+
+
+
+//watches for submit of zip form 
+function watchForm() {
+    $('form.zipCode').submit(event => {
+      event.preventDefault();
+      const inputZip = $('.js-zip').val();
+      getSuggestions(inputZip);
+
+    });
+  }
   
-//   $(watchForm);
+  $(watchForm);
 
 
-  // have a spot when shows are showing to modify date/theater? 
+//removes landingPg page, removes hidden class on zipPg
+// $('.submitDate').on('click', function() {
+//     event.preventDefault();
+//     $('.landingPg').remove();
+// })
+
+
+//tomorrow's date
+// var tomorrow = new Date();
+// tomorrow.setDate(tomorrow.getDate() + 1);
+
+//today's date
+// let today = new Date();
+// let dd = String(today.getDate()).padStart(2, '0');
+// let mm = String(today.getMonth() + 1).padStart(2, '0'); 
+// let yyyy = today.getFullYear();
+// today = mm + '-' + dd + '-' + yyyy;
+
+// document.getElementsByClassName('.today').value = today;
